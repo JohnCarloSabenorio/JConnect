6609b8f055e83a2c895368415c6b98610fafc45b
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2025-10-18 22:14:02 +0800

    commit

--- client/src/pages/NoPage.jsx
+++ client/src/pages/NoPage.jsx
@@ -1,3 +1,11 @@
 export default function NoPage() {
-  return <h1>No Page</h1>;
+  return (
+    <>
+      <div className="bg-[url(/images/backgrounds/auth-bg.jpg)] bg-no-repeat bg-cover bg-right-bottom flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col w-full bg-gray-50 md:w-xl lg:flex-row rounded-lg overflow-hidden justify-center items-center border-black shadow-2xl p-3">
+          <h1 className="text-5xl font-bold text-red-500">Not Found</h1>
+        </div>
+      </div>
+    </>
+  );
 }
